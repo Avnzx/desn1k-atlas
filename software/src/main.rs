065d4fcb85606d0ecfbe961e3054ea39d7a15331
Aavin Fernando <aavin.fernando@gmail.com>
2024-03-28 12:05:43 +1100

    [Software] basic drive updated

--- software/src/main.rs
+++ software/src/main.rs
@@ -14,34 +14,38 @@ pub mod robot;
 pub mod subsystems;
 pub mod util;
 
-// Run at 10ms intervals, AKA 100Hz
-const LOOP_TIME: Duration = Duration::from_millis(10);
+// Run at 20ms intervals, AKA 50Hz
+const LOOP_TIME: Duration = Duration::from_millis(20);
 
 fn main() {
     let mut controller = CommandSwitchController::new(None);
 
     // Create Subsystems
-    let drive_subsystem = DriveSubsystem::new();
+    let mut drive_subsystem = DriveSubsystem::new();
     let claw_subsystem = ClawSubsystem::new();
 
     let mut scheduler = command::command_scheduler::CommandScheduler {
         disabled: false,
         ..Default::default()
     };
+
+
     
     scheduler
-        .register_subsystem(&drive_subsystem)
+        // .register_subsystem(&drive_subsystem)
         .register_subsystem(&claw_subsystem);
 
     // Main loop, where everything happens
     loop {
         let loop_start = Instant::now();
+        // scheduler.run(); // SPWM hates this one trick
 
         let _ = controller.update(&mut scheduler);
-        scheduler.run();
-    
+
+        // drive_subsystem.drive_all_motors(-controller.raw.get_left_y());
+        drive_subsystem.drive_tail_normal(-controller.raw.get_left_y(), -controller.raw.get_right_y());
 
         // Loop time - time it took for this iter = time to wait until next iter
-        thread::sleep(LOOP_TIME - (Instant::now() - loop_start));
+        thread::sleep(LOOP_TIME.saturating_sub(Instant::now().duration_since(loop_start)));
     }
 }
--- software/src/subsystems/drive_subsystem.rs
+++ software/src/subsystems/drive_subsystem.rs
@@ -1,35 +1,99 @@
 use std::time::Duration;
 
-use rppal::{gpio::{Gpio, OutputPin}, pwm::{Channel, Pwm}};
+use rppal::{gpio::{Gpio, OutputPin}, pwm::{Channel, Polarity, Pwm}};
 
 use crate::command::subsystem::Subsystem;
 
 // BCM numbering https://pinout.xyz/
-const MOTOR_L: u8 = 17;
-const MOTOR_R: u8 = 27;
-const MOTOR_T: u8 = 22;
-const SERVO_T: Channel = Channel::Pwm0;
+const MOTOR_L: Channel = Channel::Pwm0;
+const MOTOR_R: Channel = Channel::Pwm1;
+const MOTOR_T: u8 = 27;
+const SERVO_T: u8 = 17;
+
+const PWMPERIOD: Duration = Duration::from_millis(10);
 
 pub struct DriveSubsystem {
-  left: OutputPin,
-  right: OutputPin,
+  left: Pwm,
+  right: Pwm,
   tail: OutputPin,
-  tailservo: Pwm
+  tailservo: OutputPin
 }
 
 impl DriveSubsystem {
     pub fn new() -> DriveSubsystem {
         DriveSubsystem {
-          left: Gpio::new().unwrap().get(MOTOR_L).unwrap().into_output(),
-          right: Gpio::new().unwrap().get(MOTOR_R).unwrap().into_output(),
+          left: Pwm::with_frequency(MOTOR_L, 100.0, 0.5, Polarity::Normal, true).unwrap(),
+          right: Pwm::with_frequency(MOTOR_R, 100.0, 0.5, Polarity::Normal, true).unwrap(),
           tail: Gpio::new().unwrap().get(MOTOR_T).unwrap().into_output(),
-          tailservo: Pwm::new(SERVO_T).unwrap()
+          tailservo: Gpio::new().unwrap().get(SERVO_T).unwrap().into_output()
         }
     }
 
     // Drive, NWU
-    pub fn drive(&mut self, forwardback: f32, leftright: f32, updown: f32, yaw: f32) {
-      self.left.set_pwm(Duration::from_micros(2000), Duration::from_micros(1000)).unwrap();
+    // pub fn drive(&mut self, pitch: f32, roll: f32, updown: f32) {
+    //   let mut left = 0.0;
+    //   let mut right = 0.0;
+    //   let mut tail = 0.0;
+
+    //   let pitch = pitch.clamp(-1.0, 1.0);
+    //   left -= pitch;
+    //   right -= pitch;
+    //   tail += pitch;
+
+    //   let roll = roll.clamp(-1.0, 1.0);
+    //   left += roll;
+    //   right -= roll;
+
+    //   let updown = updown.clamp(-1.0, 1.0);
+    //   left += updown;
+    //   right += updown;
+    //   tail += updown;
+
+
+    //   // Normalize speeds
+    //   let minimum = f32::min(f32::min(left,right),tail);
+    //   left += f32::abs(minimum); right += f32::abs(minimum); tail += f32::abs(minimum); // shift all to +ve
+
+
+    //   let maximum = f32::max(f32::max(left,right),tail);
+    //   left = left / maximum;
+    //   right = right / maximum;
+    //   tail = tail / maximum;
+
+    //   if maximum == 0.0 {
+    //       left = 0.0; right = 0.0; tail = 0.0;
+    //   }
+
+    //   println!("raw tail: {}, r: {}, l: {}", tail, right, left);
+
+
+    //   let left_duty = 0.5 + 0.5 * left.clamp(0.0, 1.0);
+    //   let right_duty = 0.5 + 0.5 * right.clamp(0.0, 1.0);
+    //   let tail_duty = 0.5 + 0.5 * tail.clamp(0.0, 1.0);
+
+    //   println!("tail: {}, r: {}, l: {}", tail_duty, right_duty, left_duty);
+
+    //   self.left.set_duty_cycle(left_duty.into()).unwrap();
+    //   self.right.set_duty_cycle(right_duty.into()).unwrap();
+
+    //   let pulse = Duration::from_secs_f64((0.01 * tail_duty).into());
+    //   self.tail.set_pwm(Duration::from_millis(10), pulse).unwrap();    }
+
+    pub fn drive_tail_normal(&mut self, tail: f32, front: f32) {
+      self.left.set_duty_cycle(Self::throttle_to_duty(front).into()).unwrap();
+      self.right.set_duty_cycle(Self::throttle_to_duty(front).into()).unwrap();
+      self.tail.set_pwm(PWMPERIOD, Self::throttle_to_pulse(PWMPERIOD, tail)).unwrap();
+    }
+
+    #[inline]
+    fn throttle_to_duty(throttle: f32) -> f32 {
+      0.5 + 0.5 * throttle.clamp(0.0, 0.99)
+    }
+
+    #[inline]
+    fn throttle_to_pulse(period: Duration, throttle: f32) -> Duration {
+      let duty = 0.5 + 0.5 * throttle.clamp(0.0, 0.99);
+      period.mul_f32(duty)
     }
 }
 
